ba9d29ef0574da482ce912b4d17309302845e357
armandoagra <armando.agra@gmail.com>
2019-12-26 17:39:31 -0300

    Criação de scripts básicos
    Scripts de movimentação, atirar, comportamento do projétil e importação de assets para o canhão e para o projétil.

---
+++ Cannon Lord V2/Assets/Scripts/CannonMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonMovement : MonoBehaviour
+{
+
+    public float speed; // Velocidade que o canhão muda o ângulo
+    public float angleLimit; // Limite do ângulo
+    float angle; // Ângulo atual
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle = Mathf.Sin(Time.time * speed); // O ângulo é determinado pelo tempo do jogo e pela velocidade escolhida. É baseada no seno desse valor.
+        transform.eulerAngles = Vector3.forward * angle * angleLimit; // Alteração do ângulo do canhão de acordo com o limite\
+    }
+}
---
+++ Cannon Lord V2/Assets/Scripts/Cannonball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonball : MonoBehaviour
+{
+
+    float directionX;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+    }
+
+    public void SetDirection(Vector3 angles)
+    {
+        transform.localEulerAngles = angles;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
